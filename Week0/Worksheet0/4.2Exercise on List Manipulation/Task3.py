30fb61559d9ee5022c8ca3236a8ce1fa1ddcf045
Aashan12 <Nagarkotiaashan1@gmail.com>
2026-03-11 00:48:02 +0545

    4.2 List Manipulation - Task 3: Reverse a List Using Slicing

---
+++ Week0/Worksheet0/4.2Exercise on List Manipulation/Task3.py
@@ -0,0 +1,15 @@
+def reverse_list(lst):
+    """
+    Reverse a list using slicing.
+
+    Parameters:
+    lst (list): Input list.
+
+    Returns:
+    list: Reversed list.
+    """
+    return lst[::-1]
+
+
+# Example
+print(reverse_list([1, 2, 3, 4, 5]))
